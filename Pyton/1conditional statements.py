6a19234f1b848b8bf7dfd1a22bc4dc51618136f0
MagyPendo <magypendo3@gmail.com>
2026-07-12 21:29:16 +0300

    initial commit

---
+++ Pyton/1conditional statements.py
@@ -0,0 +1,5 @@
+# Conditional Expression (Ternary Operator)
+# Conditional Expression (Ternary Operator) is a short way to write an if-else statement in a single line. It is used when choosing between two values based on a condition
+age = 20
+s = "Adult" if age >= 18 else "Minor"
+print(s)
